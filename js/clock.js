58fbb5173f1b132346e7a2240987fbc6ce5d8a02
yunuskaragoz <yunuskaragoz83@gmail.com>
2025-02-24 16:59:47 +0300

    Kullanıcı adı giriş kontrolü eklendi

--- js/clock.js
+++ js/clock.js
@@ -1,23 +1,47 @@
-
 var kullaniciAdi = prompt("Lütfen isminizi girin:");
+
+function girisKontrolu(kullaniciAdi) {
+  if (kullaniciAdi === null || kullaniciAdi.trim() === "") {
+      alert("İsim boş bırakılamaz!!!.");
+      return false;
+  } else {
+      alert("İsminiz: " + kullaniciAdi);
+      return true;
+  }
+}
+
+while (true) {
+  if (girisKontrolu(kullaniciAdi)) {
+      break;
+  }
+  kullaniciAdi = prompt("Lütfen isminizi girin:");
+}
+
 document.getElementById("myName").textContent = kullaniciAdi;
 
 function guncelleZaman() {
-    var saatDiv = document.getElementById("myClock");
-    var tarih = new Date();
-    var gun = tarih.toLocaleDateString("tr-TR", { weekday: "long" });
-    var saat = tarih.getHours();
-    var dakika = tarih.getMinutes();
-    var saniye = tarih.getSeconds();
-  
-    var zamanMetni = saat + ":" + (dakika < 10 ? "0" : "") + dakika + ":" + (saniye < 10 ? "0" : "") + saniye + " " + gun;
-  
-    saatDiv.textContent = zamanMetni;
-  }
-  
-  // Zamanı başlangıçta güncelle
-  guncelleZaman();
-  
-  // Her saniye zamanı güncelle
-  setInterval(guncelleZaman, 1000);
+  var tarihDiv = document.getElementById("date");
+  var saatDiv = document.getElementById("myClock");
+  var tarih = new Date();
+  var gun = tarih.toLocaleDateString("tr-TR", { weekday: "long" });
+  var ay = tarih.toLocaleDateString("tr-TR", { month: "long" });
+  var yil = tarih.getFullYear();
+  var saat = tarih.getHours();
+  var dakika = tarih.getMinutes();
+  var saniye = tarih.getSeconds();
+
+  var zamanMetni = `${saat < 10 ? "0" : ""}${saat}:${dakika < 10 ? "0" : ""}${dakika}:${saniye < 10 ? "0" : ""}${saniye}`;
+
+  var tarihMetni = `${tarih.getDate()} ${ay} ${yil} ${gun}`;
+
+  tarihDiv.textContent = tarihMetni;
+  saatDiv.textContent = zamanMetni;
+}
+
+// Zamanı başlangıçta güncelle
+guncelleZaman();
+
+// Her saniye zamanı güncelle
+setInterval(guncelleZaman, 1000);
+
   
